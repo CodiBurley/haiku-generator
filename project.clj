46f2c2f649a3afcfe9dcddd688638b6e4f20ccee
Codi Burley <idocyelrub@gmail.com>
2016-02-11 22:39:53 -0500

    changed version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject haiku "0.1.0-SNAPSHOT"
+(defproject haiku "1.0.0"
   :description "FIXME: write description"
   :url "https://github.com/CodiBurley/haiku-generator"
   :license {:name "Eclipse Public License"
